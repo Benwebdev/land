80266514a291b336449b17b39e3ded9013841ce3
Ben Line <hello@benline.co.uk>
2021-12-05 01:40:41 +0000

    Add files via upload

---
+++ pk/test/singleLineString.ts
@@ -0,0 +1,15 @@
+export function singleLineString(strings, ...values) {
+    let output = '';
+    for (let i = 0; i < values.length; i++) {
+      output += strings[i];
+    }
+    output += strings[values.length];
+  
+    // Split on newlines.
+    let lines = output.split(/(?:\r\n|\n|\r)/);
+  
+    // Rip out the leading whitespace.
+    return lines.map((line) => {
+      return line.replace(/^\s+/gm, '');
+    }).join(' ').trim();
+}
